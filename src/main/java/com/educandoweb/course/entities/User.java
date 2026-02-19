27de5d25ec2ec1196751438c7822007eb4eaeec2
igor ribeiro <igorlopes.ilr@gmail.com>
2026-02-19 08:06:05 -0300

    user entity and resource

---
+++ src/main/java/com/educandoweb/course/entities/User.java
@@ -0,0 +1,58 @@
+package com.educandoweb.course.entities;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+
+
+    private static final long serialVersionUID = 1L;
+    private Long id;
+    private String name;
+    private String phone;
+    private String password;
+
+    public User(){
+
+    }
+
+    public User(Long id, String name, String phone, String password) {
+        this.id = id;
+        this.name = name;
+        this.phone = phone;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+
+}
---
+++ src/main/java/com/educandoweb/course/resources/UserResource.java
@@ -0,0 +1,22 @@
+package com.educandoweb.course.resources;
+
+import com.educandoweb.course.entities.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/users") // responsavel por mapear as requisições http que chegarem no endpoint /users
+public class UserResource {
+
+    // aqui vai ficar os endpoints para acessar os usuários
+    @GetMapping
+    public ResponseEntity<User> findAll(){
+        User u = new User(1L, "Maria Brown", "988888888", "123456");
+        return ResponseEntity.ok().body(u); // retorna a resposta com sucesso
+
+
+    }
+
+}
