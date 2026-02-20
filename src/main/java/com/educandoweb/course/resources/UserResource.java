22fe4b424c11cece90253903d5ce54c25b98c0a1
igor ribeiro <igorlopes.ilr@gmail.com>
2026-02-20 14:03:51 -0300

    service layer, component registration

--- src/main/java/com/educandoweb/course/resources/UserResource.java
+++ src/main/java/com/educandoweb/course/resources/UserResource.java
@@ -1,22 +1,36 @@
 package com.educandoweb.course.resources;
 
 import com.educandoweb.course.entities.User;
+import com.educandoweb.course.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/users") // responsavel por mapear as requisições http que chegarem no endpoint /users
 public class UserResource {
 
+    //dependencia par ao service
+    @Autowired
+    private UserService service;
+
     // aqui vai ficar os endpoints para acessar os usuários
     @GetMapping
-    public ResponseEntity<User> findAll(){
-        User u = new User(1L, "Maria Brown", "988888888","maria@gmail.com", "123456");
-        return ResponseEntity.ok().body(u); // retorna a resposta com sucesso
+    public ResponseEntity<List<User>> findAll(){
+        List<User> list = service.findAll();
+        return ResponseEntity.ok().body(list);// retorna a resposta com sucesso
 
+    }
 
+    @GetMapping(value = "/{id}") //será uma requisição do tipo get, para acessar o id
+    public ResponseEntity<User> findById(@PathVariable Long id){
+        User obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);// retorna a resposta com sucesso
     }
 
 }
---
+++ src/main/java/com/educandoweb/course/services/UserService.java
@@ -0,0 +1,27 @@
+package com.educandoweb.course.services;
+
+import com.educandoweb.course.entities.User;
+import com.educandoweb.course.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+//Registrar como componente de serviço do Spring, para que ele possa ser injetado em outras classes, como o UserResource
+//para isso, basta colocar a anotação @Service na classe
+@Service // agora poderá ser injetado em outras classes, como o UserResource, usando a anotação @Autowired
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll() {
+        return repository.findAll(); // retorna a lista de usuários do banco de dados, usando o método findAll do user repository, que retorna uma lista de usuários
+    }
+
+    public User findById(Long id) {
+        Optional<User> obj = repository.findById(id);
+        return obj.get(); // retorna o usuário do banco de dados, usando o método findById do user repository, que retorna um Optional<User>, e o método get() para obter o usuário do Optional
+    }
+}
