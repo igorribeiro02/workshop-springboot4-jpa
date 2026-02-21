ebb79d419037bbf7ec94d23b976d40fbdaf11ced
igor ribeiro <igorlopes.ilr@gmail.com>
2026-02-21 20:27:56 -0300

    Order item, many-to-many association with extra attributes PART1

---
+++ src/main/java/com/educandoweb/course/entities/OrderItem.java
@@ -0,0 +1,78 @@
+package com.educandoweb.course.entities;
+
+import com.educandoweb.course.entities.pk.OrderItemPK;
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_order_item")
+public class OrderItem implements Serializable {
+    private  static final long serialVersionUID = 1L;
+
+
+    @EmbeddedId
+    private OrderItemPK id = new OrderItemPK();
+
+    private Integer quantity;
+
+    private Double price;
+
+    public OrderItem(){
+
+    }
+
+    public OrderItem(Order order, Product product, Integer quantity, Double price) {
+        id.setOrder(order);
+        id.setProduct(product);
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    public Order getOrder(){
+        return id.getOrder();
+    }
+    public void setOrder(Order order){
+        id.setOrder(order);
+    }
+    public Product getProduct(){
+        return id.getProduct();
+    }
+
+    public void setProduct(Product product){
+        id.setProduct(product);
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof OrderItem orderItem)) return false;
+        return Objects.equals(id, orderItem.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+    //temos que fazer o get e set do order e do product, porque o id é composto por esses dois atributos, então para acessar o order e o product, temos que acessar o id, e para acessar o id, temos que acessar o order e o product, então temos que fazer o get e set do order e do product para poder acessar o order e o product através do id
+
+}
